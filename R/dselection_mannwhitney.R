0811a382ffd6699b6d0a4a95d050f17f22dd3b22
chiaragaiamagnani <c.magnani9@campus.unimib.it>
2023-05-26 10:43:11 +0200

    added example to documentation and corrected error in dselection_mannwhitney

--- R/dselection_mannwhitney.R
+++ R/dselection_mannwhitney.R
@@ -17,6 +17,11 @@
 #' @export
 #'
 #' @examples
+#' set.seed(321)
+#' Sxy = sample(x=1:1000, size=100)
+#' Sx = sample(Sxy, size=70)
+#' Sy = setdiff(Sxy, Sx)
+#' dselection_mannwhitney(S_Y=Sy, S_X=Sx, alpha = 0.1, selection = 1:10, nonselection=11:30)
 #'
 #'
 #'
@@ -47,7 +52,7 @@ dselection_mannwhitney = function(S_Y,S_X,alpha,selection,nonselection){
   k=1; b=-1; Q=0;
 
   for(a in 1:n){
-    if(U[k+b+1]>=V[a-k-b] || a=1){
+    if(U[k+b+1]>=V[a-k-b] || a==1){
       Q=Q+U[k+b+1]
       b=b+1
     }
@@ -65,7 +70,7 @@ dselection_mannwhitney = function(S_Y,S_X,alpha,selection,nonselection){
     }
   }
 
-  return(s-k-1)
+  return(s-k+1)
 }
 
 
