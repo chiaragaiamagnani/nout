2719bbb1276b8793599a78a6d97934cf2c0dae0b
chiaragaiamagnani <c.magnani9@campus.unimib.it>
2023-05-29 11:24:13 +0200

    added function dselection_mannwhitney.R and its documentation

--- R/dselection_mannwhitney.R
+++ R/dselection_mannwhitney.R
@@ -6,8 +6,7 @@
 #' @param S_Y score vector for the test set
 #' @param S_X score vector for the calibration set
 #' @param alpha significance level
-#' @param selection set of the selected indices
-#' @param nonselection set of the not selected indices
+#' @param S set of the selected indices
 #'
 #' @return An integer which is the \eqn{(1 − \alpha)}-confidence lower bound for
 #' the number of true discoveries in closed testing procedure using
@@ -21,56 +20,34 @@
 #' Sxy = sample(x=1:1000, size=100)
 #' Sx = sample(Sxy, size=70)
 #' Sy = setdiff(Sxy, Sx)
-#' dselection_mannwhitney(S_Y=Sy, S_X=Sx, alpha = 0.1, selection = 1:10, nonselection=11:30)
+#' dselection_mannwhitney(S_Y=Sy, S_X=Sx, alpha = 0.1, S=1:10)
 #'
 #'
 #'
 #'
-dselection_mannwhitney = function(S_Y,S_X,alpha,selection,nonselection){
+#' @importFrom Rcpp sourceCpp
+#' @importFrom Rcpp evalCpp
+#' @useDynLib nout, .registration=TRUE
+#'
+dselection_mannwhitney = function(S_Y, S_X, alpha, S){
 
-  m = length(S_X)
   n = length(S_Y)
-  selected.S_Y = S_Y[selection]
-  nonselected.S_Y = S_Y[nonselection]
-  s = length(selected.S_Y)
+  m = length(S_X)
+  s = length(S)
+  notS = (1:n)[-S]
 
   crit = nout::critWMW(m=m, n=s,alpha=alpha)$crit.vals
 
-  ordered.selected.S_Y = sort(selected.S_Y, decreasing = FALSE)
+  u_j = sapply(S, function(j) sum(S_Y[j]>S_X))
+  v_j = sapply(notS, function(j) sum(S_Y[j]>S_X))
 
-  ordered.selection = sort(selection, decreasing = FALSE)
-  ordered.nonselection = sort(nonselection, decreasing = FALSE)
+  u0 = max(c(u_j,v_j))
+  v0 = min(c(u_j,v_j))
+  myu = c(v0, sort(u_j), rep(u0+1, n-s+1))
+  myv = c(v0, sort(v_j), rep(u0+1, s+1))
 
-  U_j = sapply(1:s, function(j) sum(selected.S_Y[j]>S_X))
-  V_j = sapply(1:(n-s), function(j) sum(nonselected.S_Y[j]>S_X))
-
-  v0 = max(U_j[1], V_j[1])
-  tail = rep(min(U_j[s], V_j[n-s]), times=(n-s))
-  V = c(V_j, tail)
-  U = c(U_j, tail)
-
-  k=1; b=-1; Q=0;
-
-  for(a in 1:n){
-    if(U[k+b+1]>=V[a-k-b] || a==1){
-      Q=Q+U[k+b+1]
-      b=b+1
-    }
-    else{
-      Q=Q+V[a-k-b]
-    }
-    while(k<=min(s,a) & Q>crit[n-a+1]){
-      if(b>0){
-        b=b-1
-      }
-      else{
-        Q=Q+U[k+1]-V[a-k]
-      }
-      k=k+1
-    }
-  }
-
-  return(s-k+1)
+  d = findDiscSum(s=s, m=n, u=myu, v=myv, cs=crit)
+  return(d)
 }
 
 
