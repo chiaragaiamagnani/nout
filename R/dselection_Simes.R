adecc7656137c4aa2730d181518944fb4238f846
chiaragaiamagnani <c.magnani9@campus.unimib.it>
2023-05-29 15:24:16 +0200

    update

--- R/dselection_Simes.R
+++ R/dselection_Simes.R
@@ -6,7 +6,7 @@
 #'
 #' @param S_Y : score vector for the test set
 #' @param S_X : score vector for the calibration set
-#' @param selection : vector of selected indices
+#' @param S : vector of selected indices
 #' @param alpha : significance level
 #'
 #' @return An integer which is the \eqn{(1 − \alpha)}-confidence lower bound for
@@ -20,10 +20,10 @@
 #' Sxy = sample(x=1:1000, size=100)
 #' Sx = sample(Sxy, size=70)
 #' Sy = setdiff(Sxy, Sx)
-#' dselection_Simes(S_Y=Sy, S_X=Sx, selection = 5:15)
+#' dselection_Simes(S_Y=Sy, S_X=Sx, S = 5:15)
 #'
-dselection_Simes = function(S_Y, S_X, selection, alpha = 0.1){
-  S_Y.selection = S_Y[selection]
+dselection_Simes = function(S_Y, S_X, S, alpha = 0.1){
+  S_Y.selection = S_Y[S]
   n = length(S_Y.selection)
   m = length(S_X)
   pval = sapply(1:n, function(i) (1+sum(S_X >= S_Y.selection[i]))/(m+1))
