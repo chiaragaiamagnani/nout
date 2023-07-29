44db4d2b0f3a320c0ff61d73a6fd4a8965c6dc40
chiaragaiamagnani <c.magnani9@campus.unimib.it>
2023-07-29 10:49:21 +0200

    tidy and update

--- R/d_MannWhitneyk3.R
+++ R/d_MannWhitneyk3.R
@@ -2,7 +2,9 @@
 #' d_MannWhitneyk3
 #'
 #' @description It returns the lower bound for the number of true discoveries in closed testing procedure
-#' using Wilcoxon-Mann-Whitney local test.
+#' using the test statistic \eqn{\sum_{j=1}^n U_j(U_j+1)}, where \eqn{U_j} is the rank of the
+#' \eqn{j}th observation in the test set among the vector of calibration units and \eqn{n} is the
+#' number of observations in the test set of the test set.
 #'
 #' @param S_Y : score vector for the test set
 #' @param S_X : score vector for the calibration set
@@ -11,8 +13,9 @@
 #' @return An integer which is the \eqn{(1 − \alpha)}-confidence lower bound for
 #' the number of true discoveries in closed testing procedure using
 #' the LMPI local test corresponding to \eqn{k=3} applied to conformal *p*-values.
-#' The selection set, i.e. the set of hypothesis
-#' indices that we are interested in is \eqn{[m]=:\{1,...,m\}} by default.
+#' The selection set is the set of indices corresponding to the observations in the test set that
+#' we want to test. By default, the selection set is \eqn{[n]=:\{1,...,n\}} and corresponds to the
+#' entire test set.
 #'
 #' @export
 #'
@@ -29,13 +32,12 @@ d_MannWhitneyk3 = function(S_Y,S_X,alpha){
   m = length(S_X)
 
   crit = sapply(1:n, function(h){
-    theta = m/(3*h)*(m-2)
+    theta = m/(3*h)*(m+2)
     z11 = 4/45*m^4-4/5*m^3+29/90*m^2-121/90*m-1/5
     z12 = (7*m^4+8*m^3+2*m^2-2*m)/15
-    variance = z11+z12/h
-    res = stats::qnorm(alpha, mean=theta, sd = sqrt(variance/h),
-                                              lower.tail = F)
-    return(res)
+    variance = (z11+z12/h)/sqrt(h)
+    quantiles = stats::qnorm(alpha, mean=theta, sd = sqrt(variance), lower.tail = F)
+    return(quantiles)
     })
 
   U_i = sapply(1:n, function(i) sum(S_Y[i]>S_X))
