8cb8d92009a50c4e49d39fc5fb29cc7892c11d34
chiaragaiamagnani <c.magnani9@campus.unimib.it>
2023-08-03 19:04:37 +0200

    new file to help plot in Latex

---
+++ trials/RealData/PowerStudy/FinalSimu/PlotsToBePastedInLatex.R
@@ -0,0 +1,277 @@
+
+#' @misc{CreditCard,
+#'   key = {Credit Card Fraud Detection Data Set},
+#'   note = {https://www.kaggle.com/mlg-ulb/creditcardfraud. Not normalized, without duplicates, categorial attributes removed}
+#' }
+#'
+#'
+#' %@misc{Digits,
+#'   %    key = {pendigits},
+#'   %    note = {Pen-Based Recognition of Handwritten Digits Data Set. http://odds.cs.stonybrook.edu/pendigits-dataset}
+#'   %}
+#'
+#'
+#' @misc{Shuttle,
+#'   key = {Shuttle},
+#'   note = {Statlog (Shuttle) Data Set. http://odds.cs.stonybrook.edu/shuttle-dataset}
+#' }
+#'
+#'
+#' @misc{Mammography,
+#'   key = {Mammography},
+#'   note = {http://odds.cs.stonybrook.edu/mammography-dataset/}
+#' }
+#'
+#'
+#' @misc{Cover,
+#'   key = {Covertype},
+#'   note = {http://odds.cs.stonybrook.edu/forestcovercovertype-dataset}
+#' }
+
+
+# ------------------------------ Shuttle ----------------------------------------
+
+load("~/nout/trials/RealData/PowerStudy/FinalSimu/Shuttle/resShuttle0.1v2")
+View(resShuttle0.1v2)
+results = resShuttle0.1v2$compact.results
+n=20
+n1s = seq(from = 0, to = n, by=1)
+d_BH = vector()
+d_StoBH = vector()
+d_Sim = vector()
+d_StoSimes = vector()
+d_WMW = vector()
+
+pow_BH = vector()
+pow_StoBH = vector()
+pow_Sim = vector()
+pow_StoSimes = vector()
+pow_WMW = vector()
+
+disc_Sim = vector()
+disc_StoSimes = vector()
+disc_WMW = vector()
+
+for(j in 1:length(n1s)){
+  d_BH[j] = results[[j]]$mean.lb.d[1]
+  d_StoBH[j] = results[[j]]$mean.lb.d[2]
+  d_Sim[j] = results[[j]]$mean.lb.d[3]
+  d_StoSimes[j] = results[[j]]$mean.lb.d[4]
+  d_WMW[j] = results[[j]]$mean.lb.d[5]
+
+  pow_BH[j] = results[[j]]$mean.powerGlobalNull[1]
+  pow_StoBH[j] = results[[j]]$mean.powerGlobalNull[2]
+  pow_Sim[j] = results[[j]]$mean.powerGlobalNull[3]
+  pow_StoSimes[j] = results[[j]]$mean.powerGlobalNull[4]
+  pow_WMW[j] = results[[j]]$mean.powerGlobalNull[5]
+
+  disc_Sim[j] = results[[j]]$mean.n.disc[1]
+  disc_StoSimes[j] = results[[j]]$mean.n.disc[3]
+  disc_WMW[j] = results[[j]]$mean.n.disc[4]
+}
+
+# Plot lower bound d
+df <- data.frame(
+  x = n1s,
+  BH = d_BH,
+  StoreyBH = d_StoBH,
+  Simes_CT = d_Sim,
+  StoreySimes_CT = d_StoSimes,
+  WMW_CT = d_WMW
+)
+df_long <- tidyr::pivot_longer(df, cols = -x, names_to = "group", values_to = "y")
+
+ggplot(df_long, aes(x = x, y = y, color = group)) +
+  geom_line() +
+  geom_point()+
+  scale_color_manual(values = c("black", "blue", "gray", "cyan", "red")) +
+  labs(x = "Number of outliers n1", y = "Average lower bound d") + # title = "Mean of the lower bound d on B replications") +
+  theme_minimal() +
+  theme(legend.title = element_blank())
+
+
+# Plot power
+dfpower <- data.frame(
+  x = n1s,
+  BH = pow_BH,
+  StoreyBH = pow_StoBH,
+  WMW_CT = pow_WMW
+)
+df_long_power <- tidyr::pivot_longer(dfpower, cols = -x, names_to = "group", values_to = "y")
+
+ggplot(df_long_power, aes(x = x, y = y, color = group)) +
+  geom_line() +
+  geom_point()+
+  scale_color_manual(values = c("black","blue","red")) +
+  labs(x = "Number of outliers n1", y = "Power to reject the global null") + #title = "Mean of the power on B replications") +
+  theme_minimal() +
+  theme(legend.title = element_blank())
+
+
+# Plot discoveries
+df <- data.frame(
+  x = n1s,
+  Simes_CT = disc_Sim,
+  StoreySimes_CT = disc_StoSimes,
+  #WMW.cpp_CT = disc_WMW.cpp,
+  WMW_CT = disc_WMW
+)
+df_long <- tidyr::pivot_longer(df, cols = -x, names_to = "group", values_to = "y")
+
+ggplot(df_long, aes(x = x, y = y, color = group)) +
+  geom_line() +
+  geom_point()+
+  scale_color_manual(values = c("blue", "cyan", "red")) +
+  labs(x = "Number of outliers n1", y = "Average number of discoveries")+#, title = "Mean of the number of discoveries on B replications") +
+  theme_minimal() +
+  theme(legend.title = element_blank())
+
+
+# --------------------------------- Mammography ---------------------------
+
+load("~/nout/trials/RealData/PowerStudy/FinalSimu/Mammography/resMammo0.1")
+View(resMammo0.1)
+results = resMammo0.1$compact.results
+n=20
+n1s = seq(from = 0, to = n, by=1)
+d_BH = vector()
+d_StoBH = vector()
+d_Sim = vector()
+d_StoSimes = vector()
+d_WMW = vector()
+
+pow_BH = vector()
+pow_StoBH = vector()
+pow_Sim = vector()
+pow_StoSimes = vector()
+pow_WMW = vector()
+
+
+for(j in 1:length(n1s)){
+  d_BH[j] = results[[j]]$mean.discoveries[1]
+  d_StoBH[j] = results[[j]]$mean.discoveries[2]
+  d_Sim[j] = results[[j]]$mean.discoveries[3]
+  d_StoSimes[j] = results[[j]]$mean.discoveries[4]
+  d_WMW[j] = results[[j]]$mean.discoveries[5]
+
+  pow_BH[j] = results[[j]]$mean.powerGlobalNull[1]
+  pow_StoBH[j] = results[[j]]$mean.powerGlobalNull[2]
+  pow_Sim[j] = results[[j]]$mean.powerGlobalNull[3]
+  pow_StoSimes[j] = results[[j]]$mean.powerGlobalNull[4]
+  pow_WMW[j] = results[[j]]$mean.powerGlobalNull[5]
+}
+
+# Plot lower bound d
+df <- data.frame(
+  x = n1s,
+  BH = d_BH,
+  StoreyBH = d_StoBH,
+  Simes_CT = d_Sim,
+  StoreySimes_CT = d_StoSimes,
+  WMW_CT = d_WMW
+)
+df_long <- tidyr::pivot_longer(df, cols = -x, names_to = "group", values_to = "y")
+
+ggplot(df_long, aes(x = x, y = y, color = group)) +
+  geom_line() +
+  geom_point()+
+  scale_color_manual(values = c("black", "blue", "gray", "cyan", "red")) +
+  labs(x = "Number of outliers n1", y = "Average lower bound d") + # title = "Mean of the lower bound d on B replications") +
+  theme_minimal() +
+  theme(legend.title = element_blank())
+
+
+# Plot power
+dfpower <- data.frame(
+  x = n1s,
+  BH = pow_BH,
+  StoreyBH = pow_StoBH,
+  WMW_CT = pow_WMW
+)
+df_long_power <- tidyr::pivot_longer(dfpower, cols = -x, names_to = "group", values_to = "y")
+
+ggplot(df_long_power, aes(x = x, y = y, color = group)) +
+  geom_line() +
+  geom_point()+
+  scale_color_manual(values = c("black","blue","red")) +
+  labs(x = "Number of outliers n1", y = "Power to reject the global null") + #title = "Mean of the power on B replications") +
+  theme_minimal() +
+  theme(legend.title = element_blank())
+
+
+
+# ---------------------------------- Cover -------------------------------------------
+
+load("~/nout/trials/RealData/PowerStudy/FinalSimu/Cover/resCover0.1")
+View(resCover0.1)
+results = resCover0.1$compact.results
+n=20
+n1s = seq(from = 0, to = n, by=1)
+d_BH = vector()
+d_StoBH = vector()
+d_Sim = vector()
+d_StoSimes = vector()
+d_WMW = vector()
+
+pow_BH = vector()
+pow_StoBH = vector()
+pow_Sim = vector()
+pow_StoSimes = vector()
+pow_WMW = vector()
+
+
+for(j in 1:length(n1s)){
+  d_BH[j] = results[[j]]$mean.discoveries[1]
+  d_StoBH[j] = results[[j]]$mean.discoveries[2]
+  d_Sim[j] = results[[j]]$mean.discoveries[3]
+  d_StoSimes[j] = results[[j]]$mean.discoveries[4]
+  d_WMW[j] = results[[j]]$mean.discoveries[5]
+
+  pow_BH[j] = results[[j]]$mean.powerGlobalNull[1]
+  pow_StoBH[j] = results[[j]]$mean.powerGlobalNull[2]
+  pow_Sim[j] = results[[j]]$mean.powerGlobalNull[3]
+  pow_StoSimes[j] = results[[j]]$mean.powerGlobalNull[4]
+  pow_WMW[j] = results[[j]]$mean.powerGlobalNull[5]
+}
+
+# Plot lower bound d
+df <- data.frame(
+  x = n1s,
+  BH = d_BH,
+  StoreyBH = d_StoBH,
+  Simes_CT = d_Sim,
+  StoreySimes_CT = d_StoSimes,
+  WMW_CT = d_WMW
+)
+df_long <- tidyr::pivot_longer(df, cols = -x, names_to = "group", values_to = "y")
+
+ggplot(df_long, aes(x = x, y = y, color = group)) +
+  geom_line() +
+  geom_point()+
+  scale_color_manual(values = c("black", "blue", "gray", "cyan", "red")) +
+  labs(x = "Number of outliers n1", y = "Average lower bound d") + # title = "Mean of the lower bound d on B replications") +
+  theme_minimal() +
+  theme(legend.title = element_blank())
+
+
+# Plot power
+dfpower <- data.frame(
+  x = n1s,
+  BH = pow_BH,
+  StoreyBH = pow_StoBH,
+  WMW_CT = pow_WMW
+)
+df_long_power <- tidyr::pivot_longer(dfpower, cols = -x, names_to = "group", values_to = "y")
+
+ggplot(df_long_power, aes(x = x, y = y, color = group)) +
+  geom_line() +
+  geom_point()+
+  scale_color_manual(values = c("black","blue","red")) +
+  labs(x = "Number of outliers n1", y = "Power to reject the global null") + #title = "Mean of the power on B replications") +
+  theme_minimal() +
+  theme(legend.title = element_blank())
+
+
+
+
+
--- trials/RealData/PowerStudy/FinalSimu/helpLatexPlots.R
+++ trials/RealData/PowerStudy/FinalSimu/helpLatexPlots.R
@@ -102,11 +102,9 @@ out_ind = which(dataset[,ncol(dataset)]==1)
 n = 20
 n1s = seq(from=0, to=n, by=1)
 
-d_BH = vector()
-#d_StoBH = vector()
-d_Sim = vector()
-#d_StoSimes = vector()
-d_WMW = vector()
+n.disc_Sim = vector()
+n.disc_StoreySim = vector()
+n.disc_WMW = vector()
 
 pow.rejGlob_BH = vector()
 pow.rejGlob_StoBH = vector()
@@ -127,11 +125,9 @@ for(j in 1:length(n1s)){
   lb.d_StoSimes[j] = resDigits0.1$compact.results[[j]]$mean.lb.d[4]
   lb.d_WMW[j] = resDigits0.1$compact.results[[j]]$mean.lb.d[5]
 
-  d_BH[j] = resDigits0.1$compact.results[[j]]$mean.lb.d[1]
-  #d_StoBH[j] = resDigits0.1$compact.results[[j]]$mean.lb.d[2]
-  d_Sim[j] = resDigits0.1$compact.results[[j]]$mean.n.disc[3]
-  #d_StoSimes[j] = resDigits0.1$compact.results[[j]]$mean.n.disc[4]
-  d_WMW[j] = resDigits0.1$compact.results[[j]]$mean.n.disc[4]
+  n.disc_Sim[j] = resDigits0.1$compact.results[[j]]$mean.n.disc[1]
+  n.disc_StoreySim[j] = resDigits0.1$compact.results[[j]]$mean.n.disc[3]
+  n.disc_WMW[j] = resDigits0.1$compact.results[[j]]$mean.n.disc[4]
 
   pow.rejGlob_BH[j] = resDigits0.1$compact.results[[j]]$mean.powerGlobalNull[1]
   pow.rejGlob_StoBH[j] = resDigits0.1$compact.results[[j]]$mean.powerGlobalNull[2]
@@ -165,16 +161,16 @@ View(pow.rejGlob)
 
 n.disc = matrix(nrow = (n+1), ncol = 3)
 rownames(n.disc) = as.character(seq(from=0, to=n, by=1))
-colnames(n.disc) = c("FDR-BH", "CT-Simes", "CT-WMW")
-n.disc[,1] = d_BH
-n.disc[,2] = d_Sim
-n.disc[,3] = d_WMW
+colnames(n.disc) = c("CT-Simes", "CT-StoreySimes", "CT-WMW")
+n.disc[,1] = n.disc_Sim
+n.disc[,2] = n.disc_StoreySim
+n.disc[,3] = n.disc_WMW
 View(n.disc)
 
 matrixDigits0.1 = list("lb.d.matrix" = lb.d, "pow.rejGlob.matrix" = pow.rejGlob, "n.disc" = n.disc)
 save(matrixDigits0.1, file = "~/nout/trials/RealData/PowerStudy/FinalSimu/Digits/NaturalOutlierDistribution/matrixDigits0.1")
 
-#load("C:/Users/chiar/Documents/matrixDigits0.1k2")
+#load("~/nout/trials/RealData/PowerStudy/FinalSimu/Digits/NaturalOutlierDistribution/matrixDigits0.1")
 res = matrixDigits0.1
 
 theta = length(out_ind)/nrow(dataset)
@@ -224,11 +220,9 @@ out_ind = which(dataset[,ncol(dataset)]==1)
 n = 20
 n1s = seq(from=0, to=n, by=1)
 
-d_BH = vector()
-#d_StoBH = vector()
-d_Sim = vector()
-#d_StoSimes = vector()
-d_WMW = vector()
+n.disc_Sim = vector()
+n.disc_StoreySim = vector()
+n.disc_WMW = vector()
 
 pow.rejGlob_BH = vector()
 pow.rejGlob_StoBH = vector()
@@ -249,11 +243,9 @@ for(j in 1:length(n1s)){
   lb.d_StoSimes[j] = resCreditCard0.1v2$compact.results[[j]]$mean.lb.d[4]
   lb.d_WMW[j] = resCreditCard0.1v2$compact.results[[j]]$mean.lb.d[5]
 
-  d_BH[j] = resCreditCard0.1v2$compact.results[[j]]$mean.lb.d[1]
-  #d_StoBH[j] = resCreditCard0.1v2$compact.results[[j]]$mean.lb.d[2]
-  d_Sim[j] = resCreditCard0.1v2$compact.results[[j]]$mean.n.disc[3]
-  #d_StoSimes[j] = resCreditCard0.1v2$compact.results[[j]]$mean.n.disc[4]
-  d_WMW[j] = resCreditCard0.1v2$compact.results[[j]]$mean.n.disc[4]
+  n.disc_Sim[j] = resCreditCard0.1v2$compact.results[[j]]$mean.n.disc[1]
+  n.disc_StoreySim[j] = resCreditCard0.1v2$compact.results[[j]]$mean.n.disc[3]
+  n.disc_WMW[j] = resCreditCard0.1v2$compact.results[[j]]$mean.n.disc[4]
 
   pow.rejGlob_BH[j] = resCreditCard0.1v2$compact.results[[j]]$mean.powerGlobalNull[1]
   pow.rejGlob_StoBH[j] = resCreditCard0.1v2$compact.results[[j]]$mean.powerGlobalNull[2]
@@ -286,10 +278,10 @@ View(pow.rejGlob)
 
 n.disc = matrix(nrow = (n+1), ncol = 3)
 rownames(n.disc) = as.character(seq(from=0, to=n, by=1))
-colnames(n.disc) = c("FDR-BH", "CT-Simes", "CT-WMW")
-n.disc[,1] = d_BH
-n.disc[,2] = d_Sim
-n.disc[,3] = d_WMW
+colnames(n.disc) = c( "CT-Simes", "CT-StoreySimes", "CT-WMW")
+n.disc[,1] = n.disc_Sim
+n.disc[,2] = n.disc_StoreySim
+n.disc[,3] = n.disc_WMW
 View(n.disc)
 
 
@@ -347,11 +339,9 @@ out_ind = which(dataset[,ncol(dataset)]==1)
 n = 20
 n1s = seq(from=0, to=n, by=1)
 
-d_BH = vector()
-d_StoBH = vector()
-d_Sim = vector()
-d_StoSimes = vector()
-d_WMW = vector()
+n.disc_Sim = vector()
+n.disc_StoreySim = vector()
+n.disc_WMW = vector()
 
 pow.rejGlob_BH = vector()
 pow.rejGlob_StoBH = vector()
@@ -359,12 +349,22 @@ pow.rejGlob_Sim = vector()
 pow.rejGlob_StoSimes = vector()
 pow.rejGlob_WMW = vector()
 
+lb.d_BH = vector()
+lb.d_StoBH = vector()
+lb.d_Sim = vector()
+lb.d_StoSimes = vector()
+lb.d_WMW = vector()
+
 for(j in 1:length(n1s)){
-  d_BH[j] = resShuttle0.1v2$compact.results[[j]]$mean.lb.d[1]
-  d_StoBH[j] = resShuttle0.1v2$compact.results[[j]]$mean.lb.d[2]
-  d_Sim[j] = resShuttle0.1v2$compact.results[[j]]$mean.lb.d[3]
-  d_StoSimes[j] = resShuttle0.1v2$compact.results[[j]]$mean.lb.d[4]
-  d_WMW[j] = resShuttle0.1v2$compact.results[[j]]$mean.lb.d[5]
+  lb.d_BH[j] = resShuttle0.1v2$compact.results[[j]]$mean.lb.d[1]
+  lb.d_StoBH[j] = resShuttle0.1v2$compact.results[[j]]$mean.lb.d[2]
+  lb.d_Sim[j] = resShuttle0.1v2$compact.results[[j]]$mean.lb.d[3]
+  lb.d_StoSimes[j] = resShuttle0.1v2$compact.results[[j]]$mean.lb.d[4]
+  lb.d_WMW[j] = resShuttle0.1v2$compact.results[[j]]$mean.lb.d[5]
+
+  n.disc_Sim[j] = resShuttle0.1v2$compact.results[[j]]$mean.n.disc[1]
+  n.disc_StoreySim[j] = resShuttle0.1v2$compact.results[[j]]$mean.n.disc[3]
+  n.disc_WMW[j] = resShuttle0.1v2$compact.results[[j]]$mean.n.disc[4]
 
   pow.rejGlob_BH[j] = resShuttle0.1v2$compact.results[[j]]$mean.powerGlobalNull[1]
   pow.rejGlob_StoBH[j] = resShuttle0.1v2$compact.results[[j]]$mean.powerGlobalNull[2]
@@ -379,11 +379,11 @@ lb.d = matrix(nrow = (n+1), ncol = 5)
 rownames(lb.d) = as.character(n1s)
 colnames(lb.d) = c("FDR-BH", "FDR-Storey", "CT-Simes", "CT-Storey", "CT-WMW")
 
-lb.d[,1] = d_BH
-lb.d[,2] = d_StoBH
-lb.d[,3] = d_Sim
-lb.d[,4] = d_StoSimes
-lb.d[,5] = d_WMW
+lb.d[,1] = lb.d_BH
+lb.d[,2] = lb.d_StoBH
+lb.d[,3] = lb.d_Sim
+lb.d[,4] = lb.d_StoSimes
+lb.d[,5] = lb.d_WMW
 View(lb.d)
 
 pow.rejGlob = matrix(nrow = (n+1), ncol = 5)
@@ -396,7 +396,15 @@ pow.rejGlob[,4] = pow.rejGlob_StoSimes
 pow.rejGlob[,5] = pow.rejGlob_WMW
 View(pow.rejGlob)
 
-matrixShuttle0.1 = list("lb.d.matrix" = lb.d, "pow.rejGlob.matrix" = pow.rejGlob)
+n.disc = matrix(nrow = (n+1), ncol = 3)
+rownames(n.disc) = as.character(seq(from=0, to=n, by=1))
+colnames(n.disc) = c("CT-Simes", "CT-StoreySimes", "CT-WMW")
+n.disc[,1] = n.disc_Sim
+n.disc[,2] = n.disc_StoreySim
+n.disc[,3] = n.disc_WMW
+View(n.disc)
+
+matrixShuttle0.1 = list("lb.d.matrix" = lb.d, "pow.rejGlob.matrix" = pow.rejGlob, "n.disc" = n.disc)
 save(matrixShuttle0.1, file = "~/nout/trials/RealData/PowerStudy/FinalSimu/Shuttle/matrixShuttle0.1")
 
 #load("~/nout/trials/RealData/PowerStudy/FinalSimu/Shuttle/matrixShuttle0.1")
@@ -413,6 +421,27 @@ cat(paste("(",paste(round(theta,4),
                     round(sapply(1:5,
                                  function(nc) sum( dbinom(0:n,size=n,prob=theta) * res$lb.d.matrix[,nc])),
                           4), sep=","),")"))
+
+
+cat(paste("(",paste(n1s, matrixShuttle0.1$lb.d.matrix[,1], sep=","),")"))
+cat(paste("(",paste(n1s, matrixShuttle0.1$lb.d.matrix[,2], sep=","),")"))
+cat(paste("(",paste(n1s, matrixShuttle0.1$lb.d.matrix[,3], sep=","),")"))
+cat(paste("(",paste(n1s, matrixShuttle0.1$lb.d.matrix[,4], sep=","),")"))
+cat(paste("(",paste(n1s, matrixShuttle0.1$lb.d.matrix[,5], sep=","),")"))
+
+
+cat(paste("(",paste(n1s, matrixShuttle0.1$pow.rejGlob.matrix[,1], sep=","),")"))
+cat(paste("(",paste(n1s, matrixShuttle0.1$pow.rejGlob.matrix[,2], sep=","),")"))
+cat(paste("(",paste(n1s, matrixShuttle0.1$pow.rejGlob.matrix[,3], sep=","),")"))
+cat(paste("(",paste(n1s, matrixShuttle0.1$pow.rejGlob.matrix[,4], sep=","),")"))
+cat(paste("(",paste(n1s, matrixShuttle0.1$pow.rejGlob.matrix[,5], sep=","),")"))
+
+
+cat(paste("(",paste(n1s, matrixShuttle0.1$n.disc[,1], sep=","),")"))
+cat(paste("(",paste(n1s, matrixShuttle0.1$n.disc[,2], sep=","),")"))
+cat(paste("(",paste(n1s, matrixShuttle0.1$n.disc[,3], sep=","),")"))
+
+
 
 
 
@@ -495,6 +524,18 @@ cat(paste("(",paste(round(theta,4),
                                  function(nc) sum( dbinom(0:n,size=n,prob=theta) * res$lb.d.matrix[,nc])),
                           4), sep=","),")"))
 
+cat(paste("(",paste(n1s, matrixMammo0.1$lb.d.matrix[,1], sep=","),")"))
+cat(paste("(",paste(n1s, matrixMammo0.1$lb.d.matrix[,2], sep=","),")"))
+cat(paste("(",paste(n1s, matrixMammo0.1$lb.d.matrix[,3], sep=","),")"))
+cat(paste("(",paste(n1s, matrixMammo0.1$lb.d.matrix[,4], sep=","),")"))
+cat(paste("(",paste(n1s, matrixMammo0.1$lb.d.matrix[,5], sep=","),")"))
+
+
+cat(paste("(",paste(n1s, matrixMammo0.1$pow.rejGlob.matrix[,1], sep=","),")"))
+cat(paste("(",paste(n1s, matrixMammo0.1$pow.rejGlob.matrix[,2], sep=","),")"))
+cat(paste("(",paste(n1s, matrixMammo0.1$pow.rejGlob.matrix[,3], sep=","),")"))
+cat(paste("(",paste(n1s, matrixMammo0.1$pow.rejGlob.matrix[,4], sep=","),")"))
+cat(paste("(",paste(n1s, matrixMammo0.1$pow.rejGlob.matrix[,5], sep=","),")"))
 
 
 
@@ -576,5 +617,20 @@ cat(paste("(",paste(round(theta,4),
                     round(sapply(1:5,
                                  function(nc) sum( dbinom(0:n,size=n,prob=theta) * res$lb.d.matrix[,nc])),
                           4), sep=","),")"))
+
+
+
+cat(paste("(",paste(n1s, matrixCover0.1$lb.d.matrix[,1], sep=","),")"))
+cat(paste("(",paste(n1s, matrixCover0.1$lb.d.matrix[,2], sep=","),")"))
+cat(paste("(",paste(n1s, matrixCover0.1$lb.d.matrix[,3], sep=","),")"))
+cat(paste("(",paste(n1s, matrixCover0.1$lb.d.matrix[,4], sep=","),")"))
+cat(paste("(",paste(n1s, matrixCover0.1$lb.d.matrix[,5], sep=","),")"))
+
+
+cat(paste("(",paste(n1s, matrixCover0.1$pow.rejGlob.matrix[,1], sep=","),")"))
+cat(paste("(",paste(n1s, matrixCover0.1$pow.rejGlob.matrix[,2], sep=","),")"))
+cat(paste("(",paste(n1s, matrixCover0.1$pow.rejGlob.matrix[,3], sep=","),")"))
+cat(paste("(",paste(n1s, matrixCover0.1$pow.rejGlob.matrix[,4], sep=","),")"))
+cat(paste("(",paste(n1s, matrixCover0.1$pow.rejGlob.matrix[,5], sep=","),")"))
 
 
