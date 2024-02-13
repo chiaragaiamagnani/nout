f573aea89201d93ca76575c359d30daf021f4e8b
chiaragaiamagnani <c.magnani9@campus.unimib.it>
2024-02-12 17:39:35 -0800

    New function crit.T3 implemented to compute critical values via permutation when the size of the pooled calibration and test sample is too small and normal approximation is not good enough

--- R/d_MannWhitneyk3.R
+++ R/d_MannWhitneyk3.R
@@ -1,4 +1,67 @@
 
+
+
+#' crit.T3
+#'
+#' @description Given the number of observations in the calibration set (\eqn{m}) and the number
+#' of observations in the test set (\eqn{n}), it returns the vector of critical
+#' values \eqn{U(m,h)} of the Wilcoxon-Mann-Whitney test statistic letting \eqn{h} vary between
+#'  \eqn{1} and \eqn{n}, while the size of the first sample is kept fixed equal to \eqn{m}.
+#'
+#'
+#' @param S_Y : score vector for the test set
+#' @param S_X : score vector for the calibration set
+#' @param alpha : significance level of the local test. Default value is set equal to 0.1
+#' @param B : number of interation to approximate \eqn{T_3} distribution via permutation
+#' @param seed : seed to ensure reproducible results
+#'
+#'
+#'
+#' @return A R object of class *crit.vals.info*, which is a list consisting
+#' of the following elements: \itemize{
+#' \item m number of test observations
+#' \item n number of calibration observations
+#' \item alpha significance level of the local test
+#' \item crit.val critical value of LMPI \eqn{T_3} statistic.}
+#'
+#'
+#' @export
+#'
+#'@importFrom foreach %dopar%
+#' @examples
+#' crits = crit.T3(S_X=runif(10),S_Y=runif(min=0, max=0.5, 5),alpha=0.1)
+#'
+#'
+crit.T3 = function(S_X, S_Y, alpha=0.1, B=10^3, seed = 123){
+
+  set.seed(seed)
+
+  m = length(S_X)
+  n = length(S_Y)
+
+  T3 = foreach::foreach(b = 1:B, .combine=cbind) %dopar% {
+    N=m+n
+    perm = sample(1:N, m)
+    S_Z = c(S_X,S_Y)
+    S_Z.perm = c(S_Z[perm], S_Z[-perm])
+    U = sapply(1:n, function(j) rank(c(S_Z.perm[1:m], S_Z.perm[m+j]))[m+1]-1)
+    T3 = sum(U^2 + U)
+    return(T3)
+  }
+  crit = stats::quantile(as.vector(T3), probs = 1-alpha)
+
+  res = list("m" = m, "n" = n, "crit.val" = crit, "alpha" = alpha)
+  class(res) = "crit.vals.info"
+
+  return(res)
+}
+
+
+
+
+
+
+
 #' d_MannWhitneyk3
 #'
 #' @description It returns the lower bound for the number of true discoveries in closed testing procedure
@@ -9,6 +72,10 @@
 #' @param S_Y : score vector for the test set
 #' @param S_X : score vector for the calibration set
 #' @param alpha : significance level
+#' @param n.exact : if \eqn{min\{m,n\}\leq n.exact} the critical values of the LMPI \eqn{T_3}
+#' statistic are computed via permutation. Default value is set equal to 10.
+#' @param B : number of interation to approximate \eqn{T_3} distribution via permutation
+#' @param seed : seed to ensure reproducible results
 #'
 #' @return An integer which is the \eqn{(1 − \alpha)}-confidence lower bound for
 #' the number of true discoveries in closed testing procedure using
@@ -29,19 +96,24 @@
 #'
 #'
 #'
-d_MannWhitneyk3 = function(S_Y,S_X,alpha){
+d_MannWhitneyk3 = function(S_Y,S_X,n.exact=10,B=10^3, seed=123, alpha=0.1){
   n = length(S_Y)
   m = length(S_X)
 
-  crit = sapply(1:n, function(h){
-    N = m+h
-    lambda = m/N
-    theta = (4*(2-lambda))/(3*(1-lambda)*lambda)
-    variance = 64/(45*N*lambda^3*(1-lambda)^3)
-    quantile = stats::qnorm(alpha, mean=(choose(h,2)*choose(m,2))/N*theta+h*(2*h^2-3*h+1)/6+h*(h-1)/2,
-                            sd = (choose(h,2)*choose(m,2))/N*sqrt(variance), lower.tail = F)
-    return(quantile)
-  })
+  if(min(n,m)>n.exact){
+    crit = sapply(1:n, function(h){
+      N = m+h
+      lambda = m/N
+      theta = (4*(2-lambda))/(3*(1-lambda)*lambda)
+      variance = 64/(45*N*lambda^3*(1-lambda)^3)
+      quantile = stats::qnorm(alpha, mean=(choose(h,2)*choose(m,2))/N*theta+h*(2*h^2-3*h+1)/6+h*(h-1)/2,
+                              sd = (choose(h,2)*choose(m,2))/N*sqrt(variance), lower.tail = F)
+      return(quantile)
+    })
+  }
+  if(min(n,m)<=n.exact){
+    crit = sapply(1:n, function(h) crit.T3(S_X=S_X, S_Y=S_Y[1:h], B=B, alpha=alpha, seed=seed))
+  }
 
   S_Z = c(S_X, S_Y)
   R = rank(S_Z)[(m+1):(m+n)]-1
