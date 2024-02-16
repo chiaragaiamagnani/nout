b164147c53383bdeffe1bed79ff04b378bbe153c
Matteo Sesia <sesia@marshall.usc.edu>
2024-02-15 21:29:16 -0800

    Fixed bug; missing lambda

--- R/d_Simes.R
+++ R/d_Simes.R
@@ -25,14 +25,14 @@
 #' d_simes(S_Y=Sy, storey=T, S_X=Sx)
 #'
 #'
-d_simes = function(S_Y, S_X, storey = F, alpha = 0.1){
+d_simes = function(S_Y, S_X, storey = F, lambda_storey=0.5, alpha = 0.1){
   n = length(S_Y)
   m = length(S_X)
 
   pval = sapply(1:n, function(i) (1+sum(S_X >= S_Y[i]))/(m+1))
 
   if(storey){
-    d = hommel::discoveries(hommel::hommel(pval), alpha = alpha)
+    d = hommel::discoveries(hommel::hommel(pval), alpha = alpha)    
   }
   else{
     pval = sort(pval, decreasing = F)
@@ -41,7 +41,7 @@ d_simes = function(S_Y, S_X, storey = F, alpha = 0.1){
 
     # Building the levels of Simes test with Storey estimator
     # pi.not = sapply(1:m, function(i)
-    #   (1+sum(pval[i:m]>lambda))/((m-i+1)*(1-lambda)))
+    #   (1+sum(pval[i:m]>lambda_storey))/((m-i+1)*(1-lambda_storey)))
 
     # Building the levels of Simes test with Storey estimator.
     # Storey estimator will be used in the closed testing procedure
@@ -49,7 +49,7 @@ d_simes = function(S_Y, S_X, storey = F, alpha = 0.1){
     # considered pvalues has cardinality less than or equal to 2.
 
     pi.not.highlevels = sapply(1:(m-2), function(i)
-      (1+sum(pval[i:m]>lambda))/((m-i+1)*(1-lambda)))
+      (1+sum(pval[i:m]>lambda_storey))/((m-i+1)*(1-lambda_storey)))
     pi.not = c(pi.not.highlevels,1,1)
     coeff = seq(from = m, to = 1, by = -1)
     thr = alpha/(coeff*pi.not)
